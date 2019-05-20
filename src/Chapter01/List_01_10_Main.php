c01712a549d17049746502bb3684616d6c0f202b
Yuji Uchiyama <y.uchiyama.1015@gmail.com>
2019-05-21 08:46:20 +0900

    lock, unlock のコードを実装

---
+++ src/Chapter01/List_01_10_Main.php
@@ -0,0 +1,29 @@
+<?php
+namespace YUti\Dpmt\Chapter01;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use YUti\Dpmt\Common\Mutex;
+
+class List_01_10_Main
+{
+    public static function main(array $argv)
+    {
+        echo "Testing Mutex, hit CTRL+C to exit.\n";
+
+        $threads = [];
+        $mutex = new Mutex();
+
+        foreach (range(1, 5) as $i) {
+            $threads[] = new List_01_10_UserThread($i, $mutex);
+        }
+
+        foreach ($threads as $t) {
+            $t->start();
+        }
+    }
+}
+
+if (isset($argv)) {
+    List_01_10_Main::main($argv);
+}
---
+++ src/Chapter01/List_01_10_UserThread.php
@@ -0,0 +1,38 @@
+<?php
+namespace YUti\Dpmt\Chapter01;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use YUti\Dpmt\Common\Mutex;
+
+class List_01_10_UserThread extends \Thread
+{
+    private $id;
+    private $mutex;
+
+    public function __construct($id, $mutex)
+    {
+        $this->mutex = $mutex;
+        $this->id = $id;
+    }
+
+    public function run()
+    {
+        echo "#{$this->id} BEGIN\n";
+        while (true) {
+            $this->mutex->lock();
+            try {
+                echo "#{$this->id}: locked\n";
+                sleep(1);
+                if (rand(0, 5) == 0) {
+                    echo "#{$this->id}: throws exception\n";
+                    throw new \Exception();
+                }
+            } finally {
+                $this->mutex->unlock();
+                echo "#{$this->id}: unlocked\n";
+                sleep(rand(0, 3));
+            }
+        }
+    }
+}
