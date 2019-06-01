44d9f7dea7f3241059f175397bfe95a857a61239
Yuji Uchiyama <y.uchiyama.1015@gmail.com>
2019-06-01 18:06:50 +0900

    変数名の誤りを修正

--- src/Chapter04/List_04_05_Something.php
+++ src/Chapter04/List_04_05_Something.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 class List_04_05_Something extends \Threaded
 {
-    private $iniialized;
+    private $initialized;
 
     public function __construct()
     {
