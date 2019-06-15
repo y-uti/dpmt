1a305dcc458a8645a4a78c54bea97e335e1c8402
Yuji Uchiyama <y.uchiyama.1015@gmail.com>
2019-06-15 12:07:35 +0900

    練習問題 5-7 を実装

---
+++ src/Chapter05/List_05_Ex07_Host.php
@@ -0,0 +1,34 @@
+<?php
+namespace YUti\Dpmt\Chapter05;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class List_05_Ex07_Host extends \Threaded
+{
+    private $interrupted = false;
+
+    public function interrupt()
+    {
+        $this->interrupted = true;
+    }
+
+    public function execute($count)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            if ($this->interrupted) {
+                return;
+            }
+            $this->doHeavyJob();
+        }
+    }
+
+    private function doHeavyJob()
+    {
+        echo "doHeavyJob BEGIN\n";
+        $start = microtime(true);
+        while (microtime(true) < $start + 10) {
+            // Busy loop
+        }
+        echo "doHeavyJob END\n";
+    }
+}
---
+++ src/Chapter05/List_05_Ex07_Main.php
@@ -0,0 +1,33 @@
+<?php
+namespace YUti\Dpmt\Chapter05;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class List_05_Ex07_Main
+{
+    public static function main(array $argv)
+    {
+        $host = new List_05_Ex07_Host();
+
+        $thread = new class($host) extends \Thread {
+            private $host;
+            public function __construct($host)
+            {
+                $this->host = $host;
+            }
+            public function run()
+            {
+                usleep(1000 * 15000);
+                $this->host->interrupt();
+                echo "interrupt called\n";
+            }
+        };
+        $thread->start();
+
+        $host->execute(10);
+    }
+}
+
+if (isset($argv)) {
+    List_05_Ex07_Main::main($argv);
+}
