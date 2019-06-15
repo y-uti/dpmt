cdeb9c87bdad5a890c7f25cb630a4377c642e32a
Yuji Uchiyama <y.uchiyama.1015@gmail.com>
2019-06-15 10:49:29 +0900

    練習問題の List 5-9 を実装

---
+++ src/Chapter05/List_05_09_Main.php
@@ -0,0 +1,31 @@
+<?php
+namespace YUti\Dpmt\Chapter05;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class List_05_09_Main
+{
+    public static function main(array $argv)
+    {
+        $idManager = new List_05_02_MakerThreadIdManager();
+
+        $threads = [];
+        $threads[] = new List_05_02_MakerThread(
+            'MakerThread-1',
+            new List_05_04_Table(3),
+            $idManager
+        );
+        $threads[] = new List_05_03_EaterThread(
+            'EaterThread-1',
+            new List_05_04_Table(3)
+        );
+
+        foreach ($threads as $t) {
+            $t->start();
+        }
+    }
+}
+
+if (isset($argv)) {
+    List_05_09_Main::main($argv);
+}
