1ef1c896163ba088c0fcbe13feda0097c382db59
Yuji Uchiyama <y.uchiyama.1015@gmail.com>
2019-06-16 18:43:53 +0900

    コード生成スクリプトを修正

--- bin/create.sh
+++ bin/create.sh
@@ -19,27 +19,29 @@ tmpfile=$(mktemp)
 cat <<EOS >$tmpfile
 <?php
 namespace YUti\Dpmt\__NAMESPACE__;
-
-require_once __DIR__ . '/../../vendor/autoload.php';
-
+__REQUIRE_ONCE__
 class __CLASS__
 {__MAIN_DEF__
 }__CALLMAIN__
 EOS
 
+require_once=
 maindef=
 callmain=
 if [ "${class##*_}" == "Main" ]; then
+  requireonce='\nrequire_once __DIR__ . __QUOTE__/../../vendor/autoload.php__QUOTE__;\n';
   maindef='\n    public static function main(array $argv)\n    {\n    }'
   callmain='\n\nif (isset($argv)) {\n    __CLASS__::main($argv);\n}'
 fi
 
 
 cat $tmpfile \
+| sed "s!__REQUIRE_ONCE__!$requireonce!" \
 | sed "s/__MAIN_DEF__/$maindef/" \
 | sed "s/__CALLMAIN__/$callmain/" \
 | sed "s/__NAMESPACE__/$namespace/" \
 | sed "s/__CLASS__/$class/" \
+| sed "s!__QUOTE__!'!g" \
 >$filename
 
 rm -f $tmpfile
